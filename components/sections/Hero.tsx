ebf473180464f291ed5d71995791efe77e23db19
Prince-Ranpariya <princeranpariya00@gmail.com>
2026-06-19 23:04:25 +0530

    Update Hero.tsx

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -68,7 +68,7 @@ export default function Hero() {
             {/* Avatar inline */}
             <span className="inline-flex items-center self-center" aria-hidden="true">
               <Image
-                src="public/images/prince-poster.png"
+                src="prince-avatar"
                 alt="Prince Ranpariya"
                 width={80}
                 height={80}
