d008b7b08201a69917f027ec1470b215cf7e0d68
Prince-Ranpariya <princeranpariya00@gmail.com>
2026-06-19 23:01:24 +0530

    Update Hero.tsx

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -232,7 +232,7 @@ export default function Hero() {
             }}
           >
             <Image
-              src="public/images/prince-avatar.png"
+              src="public/images/prince-poster.jpg"
               alt="Prince Ranpariya - Shopify Web Developer"
               width={901}
               height={1599}
